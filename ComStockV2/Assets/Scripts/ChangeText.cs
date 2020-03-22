4b5922a76d49cec959ded64fd44c8869d1e57a05
jorge <penaj2014@fau.edu>
2020-03-22 16:40:33 -0400

    merging of V1 into V2's template

---
+++ ComStockV2/Assets/Scripts/ChangeText.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using UnityEngine;
+using Serialization;
+
+public class ChangeText : MonoBehaviour
+{
+    Weather temp = HttpRequest.weather;
+    void Update()
+    {
+        //Debug.Log("Text: " );
+        GetComponent<TMPro.TextMeshPro>().text = temp.wind.deg.ToString();
+    }     
+}
---
+++ ComStockV2/Assets/Scripts/HttpRequest.cs
@@ -0,0 +1,36 @@
+﻿using Serialization;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class HttpRequest : MonoBehaviour
+{
+    public int count = 300;
+    static string APIkey = "272fb986fe46ba90cb194bc1aee0beed";                  //weatherAPI testing key 
+    static string ZipCode = "33414";
+    private static readonly HttpClient client = new HttpClient();
+    public static Weather weather = new Weather();
+    public async Task<float> GetTemperature_json()
+    {
+        Weather temp = new Weather();
+        client.DefaultRequestHeaders.Accept.Clear();
+        string streamTask = await client.GetStringAsync($"https://api.openweathermap.org/data/2.5/weather?zip={ZipCode}&appid={APIkey}");
+        temp = JsonUtility.FromJson<Weather>(streamTask);
+        return temp.wind.deg;
+    }
+    async void Update()
+    {
+        count += 1;
+        if (count > 300)
+        {
+            count = 0;
+            weather.wind.deg = await GetTemperature_json();
+            Debug.Log(weather.wind.deg);
+            
+        }
+    }
+
+}
